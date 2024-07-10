4fbf0f376f314e6e21fd05f956dfbe98632671e4
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2024-07-10 07:40:46 -0300

    feat: update jwt token websecurity

--- src/main/java/com/service/vix/config/WebSecurityConfig.java
+++ src/main/java/com/service/vix/config/WebSecurityConfig.java
@@ -56,77 +56,38 @@ public class WebSecurityConfig {
 		return authProvider;
 	}
 
-	/**
-	 * This method is used to create bean for AuthenticationManager
-	 * 
-	 * @author @rodolfopeixoto
-	 * @date May 31, 2023
-	 * @return AuthenticationManager
-	 * @param authConfig
-	 * @return
-	 * @throws Exception
-	 * @exception Description
-	 */
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
 		return authConfig.getAuthenticationManager();
 	}
 
-	/**
-	 * This method is used to create bean for passwordEncoder and also set that we
-	 * have used BCryptPasswordEncoder
-	 * 
-	 * @author @rodolfopeixoto
-	 * @date May 31, 2023
-	 * @return PasswordEncoder
-	 * @return
-	 * @exception Description
-	 */
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	/**
-	 * This method handle all the filters that we have used for implement security
-	 * like set authenticated URLs unauthenticated URLs and also decide that we have
-	 * used form base login and custom login logout functionality. That decides also
-	 * that we have use UsernamePasswordAuthenticationFilter
-	 * 
-	 * @author @rodolfopeixoto
-	 * @date May 31, 2023
-	 * @return SecurityFilterChain
-	 * @param http
-	 * @return
-	 * @throws Exception
-	 * @exception Description
-	 */
-	@SuppressWarnings("removal")
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
-				.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/api/auth/**", "/uauth/**",
-								"D:/Service_Vix_Docs/project_image/organization/**")
-						.permitAll()
-						.requestMatchers("/api/test/**", "/organization/add-organization",
-								"/organization/process-add-organization", "/organization/searchByName/**",
-								"/organization/orgNameExists/**", "/staff/change-password/**",
-								"/forgot-password/**")
-						.permitAll().requestMatchers("/css/**", "/js/**", "/assest/**", "/fonts/**").permitAll()
-						.anyRequest().authenticated())
-				.formLogin(form -> form.loginPage("/login").failureUrl("/login?error=true").permitAll()).logout()
+			.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/api/auth/**", "/uauth/**", "/organization/add-organization", 
+					"/organization/process-add-organization", "/organization/searchByName/**", 
+					"/organization/orgNameExists/**", "/staff/change-password/**", "/forgot-password/**", 
+					"/css/**", "/js/**", "/assest/**", "/fonts/**")
+				.permitAll()
+				.anyRequest().authenticated())
+			.formLogin(form -> form.loginPage("/login").failureUrl("/login?error=true").permitAll())
+			.logout(logout -> logout
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-				.logoutSuccessHandler(logoutSuccessHandler).invalidateHttpSession(true).deleteCookies("JSESSIONID")
-				.deleteCookies("jwtToken");
+				.logoutSuccessHandler(logoutSuccessHandler)
+				.invalidateHttpSession(true)
+				.deleteCookies("JSESSIONID", "jwtToken"));
 
 		http.authenticationProvider(authenticationProvider());
-
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
-
 }
